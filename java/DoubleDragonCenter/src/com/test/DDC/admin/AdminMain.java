6521fdab45b95a5a0fa9863211b561f224b78a67
Hyewon Jeon <55887059+prohwww@users.noreply.github.com>
2020-06-15 09:34:08 +0900

    Update AdminMain.java

--- java/DoubleDragonCenter/src/com/test/DDC/admin/AdminMain.java
+++ java/DoubleDragonCenter/src/com/test/DDC/admin/AdminMain.java
@@ -12,9 +12,7 @@ import com.test.DDC.DBUtil;
 public class AdminMain {
 	
 	private static Scanner scan = new Scanner(System.in);
-//	private String id;
-//	private String pw;
-//	private DBUtil util;
+	private static String num = null;
 	
 	public static void main(String[] args) {
 		
@@ -31,7 +29,7 @@ public class AdminMain {
 		DBUtil util = new DBUtil();
 		
 		System.out.println("============================================================");
-		System.out.println("관리자");
+		System.out.println("			[관리자 로그인]");
 		System.out.println("============================================================");
 		
 		System.out.print("id : ");
@@ -44,25 +42,27 @@ public class AdminMain {
 		Statement stat = null;
 		ResultSet rs = null;
 		
-		ArrayList<String> idList = new ArrayList<String>(10); //교사명 담을 list
-		ArrayList<String> pwdList = new ArrayList<String>(10); //주민번호 뒷자리 담을 list
+		ArrayList<String> idList = new ArrayList<String>(3); //id 담을 list
+		ArrayList<String> pwdList = new ArrayList<String>(3); //pwd 담을 list
+		
+		int flag = 0;
 		
 		try {
 			conn = util.open();
 			stat = conn.createStatement();
 			
-			String sql = "select name, substr(ssn,8) as ssn from tblTeacher";
+			String sql = "select id,pwd from tblAdmin";
 			
 			rs = stat.executeQuery(sql);
 			
-			while (rs.next()) { // 교사 테이블 이름(아이디):주민번호(비밀번호) 가져오기
+			while (rs.next()) { // 관리자 테이블 id,pwd 가져오기
 				
-				String result = rs.getString("name") + "\t" + rs.getString("ssn");
+				String result = rs.getString("id") + "\t" + rs.getString("pwd");
 				
-				idList.add(rs.getString("name"));
-				pwdList.add(rs.getString("ssn"));
+				idList.add(rs.getString("id"));
+				pwdList.add(rs.getString("pwd"));
 				
-				System.out.println(result);
+//				System.out.println(result);
 			}
 			
 			for(int i=0; i<idList.size(); i++) {
@@ -70,28 +70,41 @@ public class AdminMain {
 					if(pwdList.get(i).equals(pw)) {//pw 있을 경우
 						//로그인 성공
 						System.out.println("============================================================");
-						System.out.printf("%s 선생님으로 로그인 되었습니다.");
+						System.out.printf("관리자로 로그인 되었습니다.\n");
 						
-						//메뉴 출력
+//						flag = 1;
 						menu();
 						
 					}else {
 						System.out.println("============================================================");
 						System.out.println("비밀번호가 옳지 않습니다.");
+						break;
 					}
-					
 					
 				}else {
 					System.out.println("============================================================");
 					System.out.println("없는 id입니다.");
+					break;
 				}
 				
 			}
 			
-		
-
 			stat.close();
 			conn.close();
+		
+//			if(flag == 1) {
+//				//메뉴 출력
+//				while(true) {
+//					if(num == "0") {
+//						break;
+//					}else {
+//						menu();
+//						
+//					}
+//				}
+//			}
+
+			
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -102,18 +115,18 @@ public class AdminMain {
 	private static void menu() {
 		//메인 메뉴
 		System.out.println("============================================================");
-		System.out.println("관리자");
+		System.out.println("			[관리자]");
 		System.out.println("============================================================");
 		System.out.println("1. 교사 계정 관리");
 		System.out.println("2. 개설 과정 및 개설 과목 관리");
-		System.out.println("3. 기자재 관리");
-		System.out.println("4. 교육생 관리");
+		System.out.println("3. 교육생 관리");
+		System.out.println("4. 기자재 관리");
 		System.out.println("5. 시험 관리 및 성적 조회");
 		System.out.println("6. 출결 관리 및 출결 조회");
-		System.out.println("7. 과목 평가 관리 및 결과 조회");
+		System.out.println("7. 과목 평가 조회");
 		System.out.println("8. 취업자 조회 및 관리");
 		System.out.println();
-		System.out.println("0. 뒤로가기");
+		System.out.println("0. 로그아웃");
 		System.out.println("============================================================");
 		
 		System.out.print("입력 : ");
@@ -124,34 +137,43 @@ public class AdminMain {
 
 		case "1":
 			//교사계정관리
+			AdminTeacher at = new AdminTeacher();
+			at.printTeacher();
 			
 			break;
 		case "2":
+			//개설 과정 및 개설 과목 관리
 
 			break;
 		case "3":
-
+			//교육생 관리
+			AdminStudent as = new AdminStudent();
+			as.printMain();
 			break;
 		case "4":
+			//기자재 관리
 
 			break;
 		case "5":
+			//시험 관리 및 성적 조회
 
 			break;
-
 		case "6":
+			//출결 관리 및 출결 조회
 
 			break;
 		case "7":
-
+			//과목 평가 조회
+			AdminRating ar = new AdminRating();
+			ar.printRating();
 			break;
 		case "8":
-
-			break;
-		case "9":
+			//취업자 조회 및 관리
 
 			break;
 		case "0":
+			//로그아웃
+			num = "0";
 			return;
 
 		}
